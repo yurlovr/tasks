41fe4c7d0e795f167734829a557552cb67e6b3ca
Roman <r.yurlov@promo-bot.ru>
2020-01-09 16:52:07 +0500

    fix warning

--- libs/connection.js
+++ libs/connection.js
@@ -6,6 +6,7 @@ const config = require('../config');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 mongoose.plugin(beautifyUnique);
 
